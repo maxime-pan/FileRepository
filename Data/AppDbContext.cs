e2e1e118202eba246abdb2dcdacd9b4165b3e8d2
Maxime <maxime-pan@users.noreply.github.com>
2026-01-01 16:25:08 +0000

    Change admin username and password hashseed Admin/Admin@123 with SHA256 hash
    Updated admin credentials with new username and password hash.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,14 @@
 using FileGallery.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace FileGallery.Data;
+
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
 
     public DbSet<Admin> Admins => Set<Admin>();
     public DbSet<Category> Categories => Set<Category>();
@@ -13,14 +16,15 @@ public class AppDbContext : DbContext
     public DbSet<SiteSetting> Settings => Set<SiteSetting>();
     public DbSet<AnonymousUpload> AnonymousUploads => Set<AnonymousUpload>();
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Admin>().HasData(new Admin
         {
             AdminId = 1,
-            Username = "admin",
-            PasswordHash = "9ea338953f5d4fe4b6686dbd21b639ae4d655e23b69192acd5fd85e48557cbac"
+            Username = "Admin",
+            PasswordHash = "e86f78a8a3caf0b60d8e74e5942aa6d86dc150cd3c03338aef25b7d2d7e3acc7"
         });
         modelBuilder.Entity<Category>().HasMany(c => c.Files).WithOne(f => f.Category!).HasForeignKey(f => f.CategoryId);
         modelBuilder.Entity<Category>().HasIndex(c => c.DisplayOrder);
